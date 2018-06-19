6786731f6a7017e3bbaafa903276b4ede8cb86ad
Carolyn Rehm <carolynhrehm@gmail.com>
2018-06-19 10:16:11 -0400

    tuesday morning test commit

--- TestProject/src/main/java/com/revature/servlets/TestServlet.java
+++ TestProject/src/main/java/com/revature/servlets/TestServlet.java
@@ -24,7 +24,7 @@ public class TestServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().println("Now we say somethin else");
+		response.getWriter().print("Testing out my pipeline!!");
 	}
 
 	/**
